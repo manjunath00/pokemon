0658d94367712de8de0523cc5e5c8b160fe7319f
manjunath00 <kadrimanjunath56@gmail.com>
2021-11-07 15:18:31 +0530

    removed serve by static files

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ app.use("/api", pokemonRoutes);
 
 // if (process.env.NODE_ENV === "production") {
 app.use(express.static("client/build"));
-
+/* 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
-});
+}); */
 // }
 
 /* app.get("/", (req, res) => {
