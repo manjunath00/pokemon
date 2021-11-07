8e1c75afb857eb187596aa3c090158dd4abb49d0
manjunath00 <kadrimanjunath56@gmail.com>
2021-11-07 15:14:34 +0530

    Removed the proxy & added new backend url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 
+const endPoint = process.env.backend || '';
+
 const getAllPokemons = (pageNumber = 1, limit = 20, searchTerm = "") => {
   let endPoint = ""; 
   if (pageNumber > 1) endPoint += `page=${pageNumber}`;
   if (limit !== 20) endPoint += `limit=${limit}`;
   if (searchTerm) endPoint += `search=${searchTerm}`;
-  return axios.get(`/api/pokemons?${endPoint}`);
+  return axios.get(`${endPoint}/api/pokemons?${endPoint}`);
 };
 
 const getAPokemon = (pokemonId) => {
-  return axios.get(`/api/pokemon/${pokemonId}`);
+  return axios.get(`${endPoint}/api/pokemon/${pokemonId}`);
 };
 
 const addAPokemon = (pokemon) => {
-  return axios.post(`/api/pokemons`, pokemon);
+  return axios.post(`${endPoint}/api/pokemons`, pokemon);
 };
 
 const updateAPokemon = (pokemonId, pokemon) => {
-  return axios.patch(`/api/pokemon/${pokemonId}`, pokemon);
+  return axios.patch(`${endPoint}/api/pokemon/${pokemonId}`, pokemon);
 };
 
 export { getAllPokemons, getAPokemon, addAPokemon, updateAPokemon };
